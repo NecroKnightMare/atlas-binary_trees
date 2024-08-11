daefc046b45d8add5dbf9c3a7912363faff2e196
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-10 22:32:20 -0500

    creates new node that has no left or right child and returns null if null

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,29 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
- * 
+ *binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent node of node created
+ * @value:value inserted in node
+ * Return:pointer to new node or null if failed
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+/**new node created to size of typedef struct */
+    if (new == NULL)
+    {
+        return (NULL);
+        /**if null return null */
+    }
     
+    new->n = value;
+    /**new node will become int n */
+    new->parent = parent;
+    /**then will equal to parent pointer */
+    new->left = NULL;
+    /**will indicate that there is no left child */
+    new->right = NULL;
+    /**indicates there is no right child */
+    
+    return (new);
+    /**returns pointer to parent node */      
 }
