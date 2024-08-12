ec0f0308d15efcbb2b04fa6f6961e0f6b5f47216
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 21:50:59 +0000

    added ternary ops to get height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_h, right_h;
+	size_t left_h, right_h;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    left_h = binary_tree_height(tree->left);
-    right_h = binary_tree_height(tree->right);
+	if (tree == NULL)
+{
+		return (0);
+}
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return (left_h > right_h ? left_h : right_h) + 1;
+	return (left_h > right_h ? left_h : right_h);
 }
