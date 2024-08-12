74633cd212672ebbfe3a04af6873e4e7ec39c6e4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 21:52:05 +0000

    added betty doc

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- *binary_tree_height - 
- * @tree:
- * Return:
+ *binary_tree_height - calc height
+ * @tree: pointer to root
+ * Return: 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
