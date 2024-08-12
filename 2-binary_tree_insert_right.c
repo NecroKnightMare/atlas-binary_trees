047ac37a7e3ff4dcc831314ceddc263c823c7187
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 21:38:39 +0000

    betty issues and init new

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,30 +7,32 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+    binary_tree_t *new;
 
     if (new == NULL && parent == NULL)
     {
         return (NULL);
         /*if parent or new node is Null return Null*/
     }
+    
+	new = malloc(sizeof(binary_tree_t));
 
-    new->n = value;
-    /*malloc is the value of node*/
-    new->parent = parent;
-    /* new node goes into parent*/
-    new->right = parent->right;
-    /* new node goes into right and = parent having a right child*/
-    new->left = NULL;
-    /*new nodes right child = null*/
-    if (parent->right != NULL)
-    {
-        parent->right->parent = new;
-        /*if right child is not null right childs parent is new node*/
-    }    
+	new->n = value;
+	/*malloc is the value of node*/
+	new->parent = parent;
+	/* new node goes into parent*/
+	new->right = parent->right;
+	/* new node goes into right and = parent having a right child*/
+	new->left = NULL;
+	/*new nodes right child = null*/
+	if (parent->right != NULL)
+{
+		parent->right->parent = new;
+		/*if right child is not null right childs parent is new node*/
+}
 
-    parent->right = new;
-    /**right childs parent is new node */
-    return (new);
-    /*returns new node*/
+	parent->right = new;
+	/**right childs parent is new node */
+	return (new);
+	/*returns new node*/
 }
