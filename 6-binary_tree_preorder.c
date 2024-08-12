8f97517cbaea05799f7505deb0ceba0ec64fc93b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 16:56:14 +0000

    betty issues

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 /**
- *binary_tree_preorder
- * @tree:
- * @func:
- * Return:
+ *binary_tree_preorder - goes through tree from left to right
+ * @tree: pointer to root of tree
+ * @func: pointerto a void func call
+ * Return: void
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
+	if (tree == NULL || func == NULL)
+{
+		return;
+}
 
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
