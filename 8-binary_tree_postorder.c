f59c01744e5e3c82a103e17dd1d148b85d2a3a96
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:01:07 +0000

    betty issues

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 /**
- *binary_tree_postorder -
- * @tree
- * @func:
- * Return:
+ *binary_tree_postorder - traverse the tree from left to right
+ * @tree: pointer to the root
+ * @func: pointer to a void func call
+ * Return: void
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	if (tree == NULL || func == NULL)
+{
+		return;
+}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
