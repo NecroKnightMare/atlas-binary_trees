41049d9837a8e480be84340ce40b6686e084ff82
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:22:11 +0000

    betty issues

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 /**
- *binary_tree_sibling -  
+ *binary_tree_sibling -ecks tree to see if child nodes have siblings (l&r) 
  * @node: pointer to node of child
- * Return:
+ * Return: left and right node; null if Null
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-    {
-        return (NULL);
-    }
+	if (node == NULL || node->parent == NULL)
+{
+		return (NULL);
+}
 
-    if (node->parent->left == node)
-    {
-        return (node->parent->right);
-    }
-    
-    else if (node->parent->right == node)
-    {
-        return (node->parent->left);
-    }
-        return (NULL);
+	if (node->parent->left == node)
+{
+		return (node->parent->right);
+}
+
+	else if (node->parent->right == node)
+{
+		return (node->parent->left);
+}
+	return (NULL);
 }
