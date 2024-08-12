130061d65c16a4a4e2aa64a56b73e05fde4063cd
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:03:46 +0000

    betty issues

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 /**
- *binary_tree_depth -  
- * @tree:
- * Return:
+ *binary_tree_depth - measures how deep the tree is
+ * @tree: pointer to root node
+ * Return: size_t value of how deep tree is
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t deep;
+	size_t deep;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    for (deep = 0; tree->parent != NULL; deep++)
-    {
-        tree = tree->parent;
-    }
-    return (deep);
+	if (tree == NULL)
+{
+		return (0);
+}
+	for (deep = 0; tree->parent != NULL; deep++)
+{
+		tree = tree->parent;
+}
+	return (deep);
 }
