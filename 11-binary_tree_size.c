1eab73ebc4718ac898a6960f559081b11624ed08
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:05:45 +0000

    betty issues

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 /**
- *binary_tree_size -
- * @tree:
- * Return: 
+ *binary_tree_size - measures the size of tree
+ * @tree: pointrer to root node
+ * Return: size of tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (tree == NULL)
+{
+		return (0);
+}
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
