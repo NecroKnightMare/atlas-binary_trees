1576d87bec585b90c7d9ade1a25f40a4864af9d2
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:10:35 +0000

    betty issues

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,24 +1,23 @@
 #include "binary_trees.h"
 /**
- *binary_tree_nodes -
- * @tree:
- * Return:
+ *binary_tree_nodes - counts nodes with at least 1 child in b tree
+ * @tree: pointer to root node
+ * Return: if null 0, return count of nodes
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count;
-    
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	size_t count;
 
-    count = 0;
+	if (tree == NULL)
+{
+		return (0);
+}
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        count = 1;
-    }    
+	count = 0;
 
-    return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left != NULL || tree->right != NULL)
+{
+	count = 1;
+}
+	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
