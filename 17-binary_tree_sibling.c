9a676cf184feaf58360a9b928b8af201f196ea04
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 11:13:42 -0500

    checks if child has a sibling in tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,11 +1,24 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
- * 
+ *binary_tree_sibling -  
+ * @node: pointer to node of child
+ * Return:
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+    if (node == NULL || node->parent == NULL)
+    {
+        return (NULL);
+    }
 
+    if (node->parent->left == node)
+    {
+        return (node->parent->right);
+    }
+    
+    else if (node->parent->right == node)
+    {
+        return (node->parent->left);
+    }
+        return (NULL);
 }
