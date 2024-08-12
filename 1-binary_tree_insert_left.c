4b0da3ef8cd3c8796328f82fd78e885edfbc0ea7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 19:55:05 +0000

    fixed betty due to vs, works

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- *binary_tree_insert_left - insert left child 
+ *binary_tree_insert_left - insert left child
  * @parent: pointer of parent to left child
  * @value: value of node
  * Return: null if new is null or parent is; pointer to the created node
@@ -35,7 +35,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	parent->left->parent = new;
 	/*if left child is not null left childs parent is new node*/
-}    
+}
 
 	parent->left = new;
 	/**left childs parent is new node */
