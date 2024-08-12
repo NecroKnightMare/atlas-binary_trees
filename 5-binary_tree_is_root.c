5decece58cfae11df0ca0c334aaf3ac68822eee7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-11 20:22:42 -0500

    created file to check if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
- * 
+ *binary_tree_is_root - check if node is the root
+ * @node: node thats being checked
+ * Return: 1 if root, 0 if NULL
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-
+    if (node == NULL)
+    {
+        return (0);
+    }
+    if (node->parent == NULL)
+    {
+        return (1);
+    }
+    return (0);
 }
