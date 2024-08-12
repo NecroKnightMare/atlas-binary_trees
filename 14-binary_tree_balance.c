936baa53c2455374beb5b5c07820ecab23231483
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:13:57 +0000

    betty issues

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 /**
- *binary_tree_balance -  
- * @tree:
- * Return:
+ *binary_tree_balance - measures balance of a tree with three other func
+ * @tree: pointer to root node
+ * Return: diff of left height and right height
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_h;
-    int right_h;
+	int left_h;
+	int right_h;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+{
+		return (0);
+}
 
-    left_h = (int)binary_tree_height(tree->left);
-    right_h = (int)binary_tree_height(tree->right);
+	left_h = (int)binary_tree_height(tree->left);
+	right_h = (int)binary_tree_height(tree->right);
 
-    return (left_h - right_h);     
+	return (left_h - right_h);     
 }
