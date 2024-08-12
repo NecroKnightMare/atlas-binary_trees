189f0a7dacb9adb32138fbdeb68775fec352d256
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 16:59:06 +0000

    betty issues

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 /**
- *binary_tree_inorder -
- * @tree:
- * @func:
- * Return:
+ *binary_tree_inorder - checing tree from left to right
+ * @tree: pointer to the root
+ * @func: pointer to a void func call
+ * Return: void
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
+	if (tree == NULL || func == NULL)
+{
+		return;
+}
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
