feb205910c7394629bb102b86811dd64cb1addcc
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 10:41:12 -0500

    function that measures the balance of tree by taking the height of left and right and finding the difference

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,21 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
+ *binary_tree_balance -  
+ * @tree:
+ * Return:
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    int left_h;
+    int right_h;
 
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    left_h = (int)binary_tree_height(tree->left);
+    right_h = (int)binary_tree_height(tree->right);
+
+    return (left_h - right_h);     
 }
