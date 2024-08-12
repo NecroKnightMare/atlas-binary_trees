e4e814ea64c4672e49feb39bb19fd293514f01f1
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:07:33 +0000

    betty issues

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,19 +1,18 @@
 #include "binary_trees.h"
 /**
- *binary_tree_leaves
- * @tree
- * Return:
+ *binary_tree_leaves - counts leaves in tree
+ * @tree: pointer to root node
+ * Return: leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (tree == NULL)
+{
+		return (0);
+}
+	if (tree->left == NULL && tree->right == NULL)
+{
+		return (1);
+}
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
