d685bec9d1a84094ad0c915021ecbecb94c9feca
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:19:28 +0000

    betty issues

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 /**
- *binary_tree_is_perfect -
- * @tree:
- * Return:
+ *binary_tree_is_perfect - checks to see if all levels are filled
+ * @tree: pointer to root of node
+ * Return: size of tree
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t height, size;
+	size_t height, size;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+{
+		return (0);
+}
 
-    height = binary_tree_height(tree);
-    size = binary_tree_size(tree);
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
 
-    return (size == (size_t)(1 << (height + 1)) - 1 );
+	return (size == (size_t)(1 << (height + 1)) - 1 );
 }
