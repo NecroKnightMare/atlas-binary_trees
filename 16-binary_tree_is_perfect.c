8ddf98c5683f34a161f87a751b02d47f9817f932
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 11:01:30 -0500

    checks and compares height and size of tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,20 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
+ *binary_tree_is_perfect -
+ * @tree:
+ * Return:
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+    size_t height, size;
 
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    height = binary_tree_height(tree);
+    size = binary_tree_size(tree);
+
+    return (size == (1 << (height + 1)) - 1 );
 }
