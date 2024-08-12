57a2a1ce09761ea3580f930bbb46a44efdfe9ab9
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 21:43:15 +0000

    fixed whitetrails

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,14 +7,14 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-        /*if parent or new node is Null return Null*/
-    }
-    
+	if (parent == NULL)
+{
+		return (NULL);
+		/*if parent or new node is Null return Null*/
+}
+
 	new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
