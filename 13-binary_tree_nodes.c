d093f6b5af49dad378e08a20c85999cca18273c9
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 10:28:20 -0500

    function counts nodes with at lkeast one node in the tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,5 +6,19 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+    size_t count;
+    
+    if (tree == NULL)
+    {
+        return (0);
+    }
 
+    count = 0;
+
+    if (tree->left != NULL || tree->right != NULL)
+    {
+        count = 1;
+    }    
+
+    return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
