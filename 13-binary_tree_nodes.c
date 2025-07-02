205188ff0f6dbb0323cf63fcfd441363759b9ac4
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-02 13:32:30 -0500

    broke down to two lines

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,5 +19,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	count = 1;
 }
-	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (count + binary_tree_nodes(tree->left) + 
+	binary_tree_nodes(tree->right));
 }
