b486819222b7955eec7cb15130b4cbe4e4057d82
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-02 17:14:31 -0500

    added main  file for task 22 and file for rotating tree left, updated header file

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - left rotation program
+ * @tree: pointer to root
+ * Return: pointer to new root or Null
+ */
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+    binary_tree_t *new;
+
+    if (!tree)
+    {
+        return (NULL);
+    }
+
+    if (!tree->right)
+    {
+        return (NULL);
+    }
+
+    new = tree->right;
+    tree->right = new->left;
+
+    if (new->left)
+    {
+        new->left->parent = tree;
+    }
+
+    new->left = tree;
+    new->parent = tree->parent;
+    tree->parent = new;
+
+    return (new);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -47,4 +47,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
 
+/*Advanced prototypes*/
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+
 #endif
