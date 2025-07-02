48e796a87aead93e66a35ac44412a4a4a7d1e462
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-02 13:12:48 -0500

    renamed nodes and added helpere function to balance tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,21 +1,42 @@
 #include "binary_trees.h"
 /**
+ * tree_height - measueres height of binary tree recursively
+ * @tree: pointer to root
+ * Return: tree height or 0 if NULL
+ */
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	left = tree_height(tree->left);
+	right = tree_height(tree->right);
+
+	return (1 + (left > right ? left : right));
+}
+
+/**
  *binary_tree_balance - measures balance of a tree with three other func
  * @tree: pointer to root node
  * Return: diff of left height and right height
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_b;
-	int right_b;
+	int left_height;
+	int right_height;
 
-	if (tree == NULL)
+	if(!tree)
 {
 		return (0);
 }
 
-	left_b = tree->left ? (int)binary_tree_balance(tree->left) : 0;
-	right_b = tree->right ? (int)binary_tree_balance(tree->right) : 0;
+	left_height = tree->left ? (int)tree_height(tree->left) : 0;
+	right_height = tree->right ? (int)tree_height(tree->right) : 0;
 
-	return (left_b > right_b ? left_b : right_b);
+	return (left_height - right_height);
 }
