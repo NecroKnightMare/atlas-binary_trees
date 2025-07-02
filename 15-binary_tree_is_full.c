81d602b09c8b031cb23021ed9f693beb4b1de8f3
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-02 13:29:53 -0500

    added space between, will come back to multiple instructions on a single line wat=rning for Betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,9 +11,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 }
 
-	if(tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 {
-		return(1);
+		return (1);
 }
 
 	if (tree->left != NULL && tree->right != NULL)
