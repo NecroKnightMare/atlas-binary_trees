4c5565952d48f6379160716c2fd22edc98a0eadf
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-02 17:28:32 -0500

    running into a segmentation fault, will run in checker

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_rotate_right - rotates tree right
+ * @tree: pointer to root
+ * Return: pointer to new root or Null(fail)
+ */
+
+ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+ {
+	binary_tree_t *new;
+
+	if (!tree)
+	{
+		return (NULL);
+	}
+	if (!!tree->left)
+	{
+		return (NULL);
+	}
+
+	new = tree->left;
+	tree->left = new->right;
+
+	if (new->right)
+	{
+		new->right->parent = tree;
+	}
+
+	new->right = tree;
+	new->parent = tree->parent;
+	tree->parent = new;
+
+	return (new);
+ }
