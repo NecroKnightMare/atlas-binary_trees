22c5b3a6afb2d5ef1285621290ef2a32a624903e
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-02 17:16:41 -0500

    need to remember to switch fcrom spaces to tab when making a new file, added new line

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,29 +8,29 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    if (!tree)
-    {
-        return (NULL);
-    }
+	if (!tree)
+	{
+		return (NULL);
+	}
 
-    if (!tree->right)
-    {
-        return (NULL);
-    }
+	if (!tree->right)
+	{
+		return (NULL);
+	}
 
-    new = tree->right;
-    tree->right = new->left;
+	new = tree->right;
+	tree->right = new->left;
 
-    if (new->left)
-    {
-        new->left->parent = tree;
-    }
+	if (new->left)
+	{
+		new->left->parent = tree;
+	}
 
-    new->left = tree;
-    new->parent = tree->parent;
-    tree->parent = new;
+	new->left = tree;
+	new->parent = tree->parent;
+	tree->parent = new;
 
-    return (new);
+	return (new);
 }
